8cc89fe4efcf370b12ed8a54d037678383d51b9c
RukhlovAS <wildac89@mail.ru>
2022-10-03 15:13:34 +0300

    dz 2 c#

---
+++ lesson2/Program.cs
@@ -0,0 +1,48 @@
+﻿
+//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+void Zadacha10(){
+    Console.WriteLine("Задача 10:");
+    Console.WriteLine("Введите трехзначное число:");
+    int number = Convert.ToInt32(Console.ReadLine());
+    int numberA = number/100;
+    int numberB = number%10;
+    Console.WriteLine(numberA*10+numberB);
+}
+//Задача 13: Напишите программу, которая с помощью деления выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+void Zadacha13(){
+    Console.WriteLine("Задача 13:");
+    Console.WriteLine("Введите число:");
+    int number = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+    if (number/100 > 0)
+    {
+        while (number > 1000)
+        {
+            number = number/10;
+        }
+        Console.WriteLine("третья цифра в заданном числе " + number%10);
+    }
+    else
+    {
+        Console.WriteLine("нет третьей цифры в заданном числе");
+    }
+}
+//Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+void Zadacha15(){
+    Console.WriteLine("Задача 15:");
+    Console.WriteLine("Введите число обозначающее день недели(1-7):");
+    int number = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Это выходной?");
+    if (number == 6 || number == 7)
+    {
+        Console.WriteLine("Да");
+    }
+    else
+    {
+        Console.WriteLine("Нет");
+    }
+}
+
+//Zadacha10();
+//Zadacha13();
+Zadacha15();
+
